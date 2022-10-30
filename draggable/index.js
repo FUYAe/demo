4521c72c79c0d37b2390d9341f0325d7a4dbc816
FUYAe <f2956903402@gmail.com>
2022-10-30 19:35:19 +0800

    修复拖动边bug

--- draggable/index.js
+++ draggable/index.js
@@ -20,8 +20,8 @@ function onMouseMove(e) {
     if (onMove) {
         let boxWidth = parseFloat(oddStyle.width)
         let boxHeight = parseFloat(oddStyle.height)
-        container.style.left = `${Math.min(window.innerWidth-boxWidth,Math.max(0,e.clientX-offsetX))}px`
-        container.style.top = `${Math.min(window.innerHeight-boxHeight,Math.max(0,e.clientY-offsetY))}px`
+        container.style.left = `${Math.min(window.innerWidth - boxWidth, Math.max(0, e.clientX - offsetX))}px`
+        container.style.top = `${Math.min(window.innerHeight - boxHeight, Math.max(0, e.clientY - offsetY))}px`
 
 
     }
@@ -45,30 +45,40 @@ function onResize(e, edgeClass, moveAxis) {
      * @param {*} edgeClass 监听的边
      * @param {*} moveAxis 影响的轴（width/height)
      */
-    let oddStyle = window.getComputedStyle(container)
-    let moveDirect = parseFloat(oddStyle[moveAxis])
-    let left = parseFloat(oddStyle.left)
-    let top = parseFloat(oddStyle.top)
-    let limitX = 0
-    let limitY = 0
-    let movement = moveAxis == "width" ? e.clientX - left : e.clientY - top
-    if (edgeClass == ".edge-right" || edgeClass == ".edge-bottom") {
+    let oddStyle = window.getComputedStyle(container);
+    let moveDirect = parseFloat(oddStyle[moveAxis]);
+    let left = parseFloat(oddStyle.left);
+    let top = parseFloat(oddStyle.top);
+    let limitX = 0;
+    let limitY = 0;
+    let movement = moveAxis === "width" ? e.clientX - left : e.clientY - top;
+    if (edgeClass === ".edge-right" || edgeClass === ".edge-bottom") {
         // 使用Math.max限定范围
-        container.style[moveAxis] = `${Math.min(edgeClass ==
-            ".right"?window.innerWidth-left:window.innerHeight-top,Math.max(200,-movement))}px`
+        container.style[moveAxis] = `${Math.min(
+            edgeClass === ".edge-right"
+                ? window.innerWidth - left
+                : window.innerHeight - top,
+            Math.max(200, movement)
+        )}px`;
     }
-    if (edgeClass == ".edge-top" || edgeClass == ".edge-left") {
-        let movement = moveAxis == "width" ? left - Math.max(limitX, e.clientX) : top - Math.max(limitY, e.clientY)
-        container.style[moveAxis] = `${Math.max(200,moveDirect+movement)}px`
-        if (moveAxis == "width") {
-            container.style.left = `${Math.max(limitX,e.clientX)}px`
-        } else if (moveAxis == "height") {
-            container.style.top = `${Math.max(limitY,e.clientY)}px`
-
+    if (edgeClass === ".edge-top" || edgeClass === ".edge-left") {
+        let movement =
+            moveAxis === "width"
+                ? left - Math.max(limitX, e.clientX)
+                : top - Math.max(limitY, e.clientY);
+        container.style[moveAxis] = `${Math.max(
+            200,
+            moveDirect + movement
+        )}px`;
+        if (moveAxis === "width") {
+            container.style.left = `${Math.max(limitX, e.clientX)}px`;
+        } else if (moveAxis === "height") {
+            container.style.top = `${Math.max(limitY, e.clientY)}px`;
         }
     }
-
 }
+
+
 
 
 //使出口可以改变大小
@@ -111,31 +121,31 @@ function onResizeCorner(e, cornerClass) {
     let mY = e.clientY - top
     let mXIn = left - Math.max(limitX, e.clientX)
     let mYIn = top - Math.max(limitY, e.clientY)
-    if (cornerClass == ".corner-rt") {
-        container.style.top = `${Math.max(limitY,e.clientY)}px`
-        container.style.width = `${Math.max(200,mX)}px`
-        container.style.height = `${Math.max(200,height+mYIn)}px`
+    if (cornerClass === ".corner-rt") {
+        container.style.top = `${Math.max(limitY, e.clientY)}px`
+        container.style.width = `${Math.max(200, mX)}px`
+        container.style.height = `${Math.max(200, height + mYIn)}px`
         return
     }
-    if (cornerClass == ".corner-lt") {
+    if (cornerClass === ".corner-lt") {
 
-        container.style.top = `${Math.max(limitX,e.clientY)}px`
-        container.style.left = `${Math.max(limitX,e.clientX)}px`
-        container.style.width = `${Math.max(200,width+mXIn)}px`
-        container.style.height = `${Math.max(200,height+mYIn)}px`
-
-        return
-    }
-    if (cornerClass == ".corner-lb") {
-        container.style.left = `${Math.max(limitX,e.clientX)}px`
-        container.style.width = `${Math.max(200,width+mXIn)}px`
-        container.style.height = `${Math.max(200,mY)}px`
+        container.style.top = `${Math.max(limitX, e.clientY)}px`
+        container.style.left = `${Math.max(limitX, e.clientX)}px`
+        container.style.width = `${Math.max(200, width + mXIn)}px`
+        container.style.height = `${Math.max(200, height + mYIn)}px`
 
         return
     }
+    if (cornerClass === ".corner-lb") {
+        container.style.left = `${Math.max(limitX, e.clientX)}px`
+        container.style.width = `${Math.max(200, width + mXIn)}px`
+        container.style.height = `${Math.max(200, mY)}px`
 
-    container.style.width = `${Math.min(window.innerWidth-left,Math.max(200,mX))}px`
-    container.style.height = `${Math.min(window.innerHeight-top,Math.max(200,mY))}px`
+        return
+    }
+
+    container.style.width = `${Math.min(window.innerWidth - left, Math.max(200, mX))}px`
+    container.style.height = `${Math.min(window.innerHeight - top, Math.max(200, mY))}px`
 
 
 
